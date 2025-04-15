d142037c69e7c429fd2d4cd9392de3a28ffeaf32
Broc Buhler <brocbuhler@gmail.com>
2025-04-14 17:53:17 -0700

    basic api calls added

---
+++ src/api/orgData.js
@@ -0,0 +1,72 @@
+import { clientCredentials } from '../utils/client';
+
+const endpoint = clientCredentials.databaseURL;
+
+const getOrg = (uid) =>
+  new Promise((resolve, reject) => {
+    fetch(`${endpoint}/organizations.json?orderBy="uid"&equalTo="${uid}"`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => resolve(Object.values(data)))
+      .catch(reject);
+  });
+
+const deleteOrg = (firebaseKey) =>
+  new Promise((resolve, reject) => {
+    fetch(`${endpoint}/organizations/${firebaseKey}.json`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch(reject);
+  });
+
+const getSingleOrg = (firebaseKey) =>
+  new Promise((resolve, reject) => {
+    fetch(`${endpoint}/organizations/${firebaseKey}.json`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch(reject);
+  });
+
+const createOrg = (payload) =>
+  new Promise((resolve, reject) => {
+    fetch(`${endpoint}/organizations.json`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch(reject);
+  });
+
+const updateOrg = (payload) =>
+  new Promise((resolve, reject) => {
+    fetch(`${endpoint}/organizations/${payload.firebaseKey}.json`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch(reject);
+  });
+
+export { getOrg, createOrg, deleteOrg, getSingleOrg, updateOrg };
---
+++ src/api/subData.js
@@ -0,0 +1,86 @@
+import { clientCredentials } from '../utils/client';
+// API CALLS FOR BOOKS
+
+const endpoint = clientCredentials.databaseURL;
+
+const getSub = (uid) =>
+  new Promise((resolve, reject) => {
+    fetch(`${endpoint}/subscriptions.json?orderBy="uid"&equalTo="${uid}"`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => resolve(Object.values(data)))
+      .catch(reject);
+  });
+
+const deleteSub = (firebaseKey) =>
+  new Promise((resolve, reject) => {
+    fetch(`${endpoint}/subscriptions/${firebaseKey}.json`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch(reject);
+  });
+
+const getSingleSub = (firebaseKey) =>
+  new Promise((resolve, reject) => {
+    fetch(`${endpoint}/subscriptions/${firebaseKey}.json`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch(reject);
+  });
+
+const createSub = (payload) =>
+  new Promise((resolve, reject) => {
+    fetch(`${endpoint}/subscriptions.json`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch(reject);
+  });
+
+const updateSub = (payload) =>
+  new Promise((resolve, reject) => {
+    fetch(`${endpoint}/subscriptions/${payload.firebaseKey}.json`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch(reject);
+  });
+
+const getSubByOrg = (firebaseKey) =>
+  new Promise((resolve, reject) => {
+    fetch(`${endpoint}/subscriptions.json?orderBy="organizationId"&equalTo="${firebaseKey}"`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => resolve(Object.values(data)))
+      .catch(reject);
+  });
+
+export { getSub, createSub, deleteSub, getSingleSub, updateSub, getSubByOrg };
